aac55b169cbb83a44adf4964991c7e162589ac90
Juanjo <j.alonso@eurodivisas.com>
2022-09-13 13:24:19 +0200

    Estilizando el módulo

--- api/components/src/module.vue
+++ api/components/src/module.vue
@@ -1,12 +1,22 @@
 <template>
 	<private-view title="Componentes">
-		<div :style="{ color: 'red', fontSize: '30px' }">{{subtitulo}}</div>
-		<v-list>
-			<v-list-item v-for="item in items" :key="item.id">
-				<v-image :src="'http://localhost:8055/assets/' + item.id" :alt="item.title" />
-				{{item.title}}
-			</v-list-item>
-		</v-list>
+		<div :style="styleMain">
+			<div :style="styleHeader">
+				<v-icon name="box" left :style="styleIcon" />
+				<h2 :style="styleH2">{{subtitulo}}</h2>
+			</div>
+			<v-list :style="styleGrid">
+				<v-list-item v-for="item in items" :key="item.id" :style="styleItem">
+					<div :style="styleCard">
+						<v-image :src="'http://localhost:8055/assets/' + item.id" :alt="item.title" :style="styleImage" />
+					</div>
+					<div :style="styleDetails">
+						<p :style="styleText">{{item.title}}</p>
+						<p :style="styleTextLight">Disponible</p>					
+					</div>
+				</v-list-item>				
+			</v-list>
+		</div>
 	</private-view>
 </template>
 
@@ -14,6 +24,65 @@
 export default {
 	data () {
 		return {
+			styleMain: {
+				margin: '0 42px'
+			},
+			styleHeader: {
+				display: 'flex',
+				padding: '12px 0',
+				borderTop: '1px solid #e5e7eb',
+				borderBottom: '1px solid #e5e7eb'
+			},
+			styleIcon: {
+				color: '#a2b5cd',
+				fontSize: '16px'
+			},
+			styleH2: {
+				color: '#a2b5cd',
+				fontSize: '16px'
+			},
+			styleGrid: {
+				display: 'grid',
+				gap: '1.5rem',    
+				gridTemplateColumns: 'repeat(auto-fill, minmax(12rem, 1fr))',
+				margin: '32px 0'
+			},
+			styleImage: {
+				maxWidth: '100%',
+				height: 'auto'
+			},
+			styleItem: {
+				display: 'flex',
+				flexDirection: 'column',
+				justifyContent: 'center',
+				alignItems: 'center'
+			},
+			styleCard: {
+				backgroundColor: '#f0f4f9',
+				backgroundPosition: 'center',
+				backgroundRepeat: 'no-repeat',
+				backgroundSize: 'cover',      
+				height: '12rem',
+				width: '12rem',
+				display: 'flex',
+				borderRadius: '10px',
+				flexDirection: 'column',
+				justifyContent: 'center',
+				alignItems: 'center'
+			},
+			styleDetails: {
+				display: 'flex',
+				flexDirection: 'column',
+				justifyContent: 'flex-start',
+				justifySelf: 'flex-start',
+				width: '100%'
+			},
+			styleText: {
+				paddingTop: '10px'
+			},
+			styleTextLight: {
+				color: '#a2b5cd'
+			},
 			subtitulo: "Componentes disponibles",
 			items: []
 		}
@@ -25,7 +94,7 @@ export default {
 	},
 	inject: ["api"],
 	async mounted () {		
-		const response = await this.api.get("/files?fields[]=id&fields[]=title");
+		const response = await this.api.get("/files?filter[folder][_eq]=bb6877e4-25ea-4b6f-8c8b-0e963c01570e&fields[]=id&fields[]=title");
 		this.items = response.data.data;
 	}
 };
